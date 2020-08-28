77c1a0787778e55376eb084122f68f4b8d524e8a
Alan Lu <alanzhihaolu@gmail.com>
2020-08-28 17:03:37 -0400

    Updates

---
+++ precache-manifest.814974640cd6a640d6cec68273cb0143.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5374a4b3600cebec82bf31d01600b40",
+    "url": "/PokeAPI-Practice/index.html"
+  },
+  {
+    "revision": "3c9f5e2c07e5d6f7d45f",
+    "url": "/PokeAPI-Practice/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "18a49b6404cd8ee34aa2",
+    "url": "/PokeAPI-Practice/static/css/main.8a258b40.chunk.css"
+  },
+  {
+    "revision": "3c9f5e2c07e5d6f7d45f",
+    "url": "/PokeAPI-Practice/static/js/2.21e68989.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/PokeAPI-Practice/static/js/2.21e68989.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18a49b6404cd8ee34aa2",
+    "url": "/PokeAPI-Practice/static/js/main.35907dcc.chunk.js"
+  },
+  {
+    "revision": "9635675fc8b4d6b5f330",
+    "url": "/PokeAPI-Practice/static/js/runtime-main.f2f08b91.js"
+  }
+]);
